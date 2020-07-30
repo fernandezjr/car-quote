d6897e5868957f9cc83bc8521653ae93237b42df
Rodrigo Fernández <rfernandez@exisoft.com.ar>
2020-07-30 13:49:59 -0300

    Segundo commit

---
+++ CarQuote/src/main/java/com/sysone/carquote/App.java
@@ -0,0 +1,13 @@
+package com.sysone.carquote;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
